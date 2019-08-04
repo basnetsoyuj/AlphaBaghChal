ddb6a8fa498c0a575223fc66a2df2937812d931b
SoyujBasnet <bsoyuj@gmail.com>
2019-08-04 22:09:48 +0545

    lowecase files

---
+++ game.py
@@ -0,0 +1,89 @@
+from baghchal.env import Board
+import numpy as np
+from player import HumanPlayer,MCTSPlayer
+from lookup_table import action_space
+class Game:
+    def __init__(self, board=Board()):
+        self.board = board
+
+    def start_play(self, GoatPlayer, BaghPlayer, show=True):
+        states=[]
+        mcts_probs=[]
+        value=[]
+        """start a game between two players"""
+        if show:
+            self.board.lightweight_show_board()
+        while True:
+            player_to_move = GoatPlayer if self.board.next_turn == "G" else BaghPlayer
+            states.append(self.board.board_repr())
+            player_index = 1 if self.board.next_turn == "G" else -1
+            value.append(player_index)
+            if player_to_move.__class__==HumanPlayer:
+                move = player_to_move.get_action(self.board)
+                a=np.zeros(217)
+                if len(move)==3:
+                    a[action_space[move[1:]]]=1
+                else:
+                    a[action_space[move[-4:]]]=1
+                mcts_probs.append(a)
+                self.board.safe_move(move)
+            else:
+                move, a = player_to_move.get_action(self.board,return_prob=1)
+                self.board.pure_move(move)
+                mcts_probs.append(a)
+            if show:
+                self.board.lightweight_show_board()
+            end = self.board.is_game_over()
+            if end:
+                value=np.array(value)
+                if self.board.winner() == "B":
+                    value *= -1
+                elif self.board.winner() == 0:
+                    value *= 0
+                return zip(states, mcts_probs, value)
+
+    def start_self_play(self, player, show=1, temp=1e-3,greedy_player=None,who_greedy=""):
+        """ start a self-play game using a MCTS player, reuse the search tree,
+        and store the self-play data: (state, mcts_probs, z) for training
+        """
+        self.board=Board()
+        states, mcts_probs, value = [], [], []
+        while True:
+            if greedy_player:
+                if who_greedy=="B":
+                    player_to_move = player if self.board.next_turn == "G" else greedy_player
+                elif who_greedy=="G":
+                    player_to_move = player if self.board.next_turn == "B" else greedy_player
+                else:
+                    player_to_move=greedy_player
+            else:
+                player_to_move = player
+            move, move_probs = player_to_move.get_action(self.board,
+                                                 temp=temp,
+                                                 return_prob=1)
+            # store the data
+            states.append(self.board.board_repr())
+            mcts_probs.append(move_probs)
+
+            # temporarily store 1 for goat move and -1 for bagh
+            # later can multiply,ie if goat is winner <value>*1
+            # if bagh wins <value>*-1 , if draw *0
+            player_index = 1 if self.board.next_turn == "G" else -1
+            value.append(player_index)
+
+            # perform a move
+            self.board.pure_move(move)
+            if show:
+                self.board.lightweight_show_board()
+            end = self.board.is_game_over()
+            if end:
+                value=np.array(value)
+                # winner from the perspective of the current player of each state
+                if self.board.winner() == "B":
+                    value *= -1
+                elif self.board.winner() == 0:
+                    value *= 0
+                # reset MCTS root node
+                player.reset_player()
+                w=self.board.winner()
+                return w, zip(states, mcts_probs, value)
---
+++ player.py
@@ -0,0 +1,60 @@
+from monte_carlo_tree_search import MCTS
+from baghchal.lookup_table import action_space
+import numpy as np
+
+class HumanPlayer:
+    def __init__(self):
+        pass
+
+    def get_action(self, board):
+        while True:
+            move = input("Enter your move: ")
+            try:
+                board.validate(move)
+                return move
+            except Exception as e:
+                print(e)
+
+class MCTSPlayer:
+
+    def __init__(self, policy_value_fn,
+                 cpuct=5, n_playout=2000, is_selfplay=0):
+        self.mcts = MCTS(policy_value_fn, cpuct, n_playout)
+        self.is_selfplay = is_selfplay
+
+    def reset_player(self):
+        self.mcts.update_with_move(-1)
+
+    def get_action(self, board, temp=1e-3, return_prob=0):
+        sensible_moves = board.possible_moves()
+        # the pi vector returned by MCTS as in the alphaGo Zero paper
+        move_probs = np.zeros(217)
+        if sensible_moves:
+            acts, probs = self.mcts.get_move_probs(board, temp)
+            counter=0
+            for act in acts:
+                index=action_space[act]
+                move_probs[index]=probs[counter]
+                counter+=1
+
+            if self.is_selfplay:
+                # add Dirichlet Noise for exploration (needed for
+                # self-play training)
+                move = np.random.choice(acts,p=
+                0.75* probs + 0.25 *
+                  np.random.dirichlet(0.3 * np.ones(len(probs))))
+                # update the root node and reuse the search tree
+                self.mcts.update_with_move(move)
+            else:
+                # with the default temp=1e-3, it is almost equivalent
+                # to choosing the move with the highest prob
+                move = np.random.choice(acts, p=probs)
+                # reset the root node
+                self.mcts.update_with_move(-1)
+
+            if return_prob:
+                return move, move_probs
+            else:
+                return move
+        else:
+            print("WARNING ! Game is over.")
